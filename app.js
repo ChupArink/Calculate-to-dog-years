e761bafb5f27d6af6f0aa027884e7cd3a92a3a52
Arink <aria.inkmaker@gmail.com>
2022-10-09 20:04:21 +0330

    V1

---
+++ app.js
@@ -0,0 +1,10 @@
+let myAge = (new Date().getFullYear() ) - 1995;
+let earlyYears = 2;
+earlyYears *= 10.5;
+laterYears = myAge - 2;
+laterYears *= 4;
+myAgeInDogYears = laterYears + earlyYears ;
+myName = 'Aria'.toLowerCase();
+console.log(`My name is ${myName}.
+I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+)
